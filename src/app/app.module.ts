6294e02b940a0111a9f1e1a26f76d5485afd00a2
Claudia Hernandez <hernandez.clauds@gmail.com>
2018-05-22 12:40:24 -0400

    added signup/login/components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from "@angular/router";
+import { RouterModule, Routes } from '@angular/router';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ProductlistService } from './services/products.service';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'index', pathMatch: 'full' },
   { path: 'index',  component: ProductListComponent },
   { path: 'product/:id', component: ProductDetailsComponent },
-]
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     HttpModule,
