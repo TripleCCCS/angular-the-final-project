41859ba1580ab38e61c24c0391120f0e5b16f23d
Claudia Hernandez <hernandez.clauds@gmail.com>
2018-05-24 16:06:42 -0400

    added home pg component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 
 const routes: Routes = [
@@ -31,7 +32,8 @@ const routes: Routes = [
     SignupComponent,
     LoginComponent,
     UserProfileComponent,
-    ShoppingCartComponent
+    ShoppingCartComponent,
+    HomePageComponent
 
   ],
   imports: [
