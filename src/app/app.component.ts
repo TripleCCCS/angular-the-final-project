2822a2784b0d9fa3b52d7ce9961a5a0c6b06486b
Crystal Meneses <crystalm927@gmail.com>
2018-05-26 00:17:50 -0400

    add items to shopping cart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { $ } from 'protractor';
 import { Pipe, PipeTransform } from '@angular/core';
 import { FilterPipe } from '../app/pipes/filter.pipe';
+import { OuthService } from '../app/services/outh.service';
+
 // import * as $ from 'jquery';
 
 
@@ -10,9 +12,25 @@ import { FilterPipe } from '../app/pipes/filter.pipe';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit {
+  user: any;
   title = 'app';
+
+  constructor( private myAuth: OuthService ){
+
+  }
+
+  ngOnInit() {
+    this.myAuth.isLoggedIn()
+    .toPromise()
+    .then( () => {
+      this.user = this.myAuth.currentUser;
+      console.log("helloooooooo: ", this.user);
+    } )
+    .catch( err => {
+      console.log("error in app comp: ", err);
+    });
+  }
 }
 
 
--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
 
+import { Component, OnInit } from '@angular/core';
+import { OuthService } from '../services/outh.service';
+import { RouterModule, Routes } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -8,13 +11,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartComponent implements OnInit {
 
-  cartOrder: Array<any> = [];
+  user: any;
+  newProduct: any = {name: '', description: '', id: '', category: '', price: '', material: '', color: '', quantity: ''};
+  productsList: Array <any> = [];
+  cartList: any = [];
 
-  addToCart(theOrder) {
-   this.cartOrder.unshift(theOrder);
+  constructor( private myService: OuthService,
+    private myRouter: RouterModule,
+    private myActivated: ActivatedRoute ) { }
+
+  ngOnInit() {
+    this.myService.isLoggedIn()
+    .toPromise()
+    .then(() => {
+      // this.user = JSON.parse(this.myService.currentUser._body);
+      this.user = this.myService.currentUser;
+
+      console.log('user in the component: ', this.user);
+    })
+    .catch();
+
+    this.myActivated.params
+    .subscribe((theParams) => {
+      const userId = theParams.id;
+      this.showTheCart(userId);
+    });
+
+    // this.showTheCart(userId);
   }
 
-  constructor() { }
+  // addNewCard(newCard) {
+  //   console.log('am i here? =======', this.newCard);
+  //   this.myService.cardInfo(this.newCard)
+  //   .toPromise()
+  //   .then( () => {
+  //     // console.log('card in the TS file: ', newCard);
+  //     this.newCard = {cardname: '', cardnumber: '', mailing_address: '', cardexp: '', cvv: '', city: '', state: '', zip: ''};
+  //     this.myRouter.navigate(['/profile']);
+  //   } )
+  //   .catch( err => {
+  //     console.log('err in component when saving card ', err);
+  //   });
+  // }
 
-  ngOnInit() { }
+  showTheCart(userId) {
+    this.myService.getTheCartContent(userId)
+    .subscribe( arrayOfProducts => {
+      this.cartList = arrayOfProducts;
+      console.log('array in the component: ', arrayOfProducts );
+    });
+  }
+
 }
+
+
